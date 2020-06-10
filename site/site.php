9435001a48249b24b73171713ac10f47fc166ff0
juan-pereira <53612511+juan-pereira@users.noreply.github.com>
2020-06-10 00:26:31 -0300

    Alteração da página principal do site

--- site/site.php
+++ site/site.php
@@ -40,6 +40,24 @@ require("../php/session.php");
 	</header>
 
 	<main>
+		<div class="input-field col s12">
+			<select>
+				<option value="" disabled selected>Selecione o mês desejado</option>
+				<option value="1">Janeiro</option>
+				<option value="2">Fevereiro</option>
+				<option value="3">Março</option>
+				<option value="4">Abril</option>
+				<option value="5">Maio</option>
+				<option value="6">Junho</option>
+				<option value="7">Julho</option>
+				<option value="8">Agosto</option>
+				<option value="9">Setembro</option>
+				<option value="10">Outubro</option>
+				<option value="11">Novembro</option>
+				<option value="12">Dezembro</option>
+			</select>
+			<label>Mês</label>
+		</div>
 		<div class="row form-center">
 			<div class="input-field col s12 m6 l3">
 				<input id="salario" type="text" class="validate ValoresItens">
