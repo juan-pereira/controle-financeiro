e812122d2b27cafe7e25af6acf99c0304a56bc13
Gustavo Martins <gustavommctg@gmail.com>
2020-06-10 13:58:32 -0300

    corrigindo a conexão

--- php/conexao.php
+++ php/conexao.php
@@ -1,3 +1,3 @@
 <?php 
         //Arquivo de conexao com a sua base de dados para se repetir apenas uma vez !
-        $con = mysqli_connect("localhost", "root", "100420", "financeiro") or die("Sem conexão com o servidor.");
+        $con = mysqli_connect("localhost", "root", "", "financeiro") or die("Sem conexão com o servidor.");
